9d4b517741d846a4753e0c987be5ca138f71e4b3
william-le1004 <kienle4456@gmail.com>
2025-02-23 21:08:36 +0700

    Revert "[DTP] Create Model"
    This reverts commit 0cc23eef4ab2e48d4814e5e4692ce0ed63924d82.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,13 +1,9 @@
-using Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddInfrastructureService(builder.Configuration);
 
 var app = builder.Build();
 
@@ -19,3 +15,30 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
--- Domain/Common/AuditEntity.cs
+++
@@ -1,6 +0,0 @@
-namespace Domain.Common;
-
-public class AuditEntity
-{
-    public string Id { get; set; }
-}
