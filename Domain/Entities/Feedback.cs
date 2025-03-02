cdd8a5631c52947ca8104dd72128023b82c1e09b
william-le1004 <kienle4456@gmail.com>
2025-03-02 12:15:26 +0700

    [Authenticate] - Remove redundance code

--- Domain/Entities/Feedback.cs
+++ Domain/Entities/Feedback.cs
@@ -10,16 +10,6 @@ public partial class Feedback : AuditEntity
 
     public string? Description { get; set; }
 
-    public DateTime? CreatedAt { get; set; }
-
-    public string? CreatedBy { get; set; }
-
-    public DateTime? LastModified { get; set; }
-
-    public string? LastModifiedBy { get; set; }
-
-    public sbyte? IsDeleted { get; set; }
-
     public virtual Tour Tour { get; set; } = null!;
 
     public virtual User User { get; set; } = null!;
