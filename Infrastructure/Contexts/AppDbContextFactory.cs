dd51d1d200683274d7a3e9b5c4eda93fc0033580
william-le1004 <kienle4456@gmail.com>
2025-03-13 22:59:21 +0700

    [Fix] - Update Connection String

--- Infrastructure/Contexts/AppDbContextFactory.cs
+++ Infrastructure/Contexts/AppDbContextFactory.cs
@@ -9,7 +9,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<DtpDbContext>
     public DtpDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DtpDbContext>();
-        optionsBuilder.UseMySQL("Server=localhost;Port=3306;Database=DTP;Uid=root;Pwd=root;");
+        optionsBuilder.UseMySQL("Server=MYSQL1001.site4now.net;Database=db_ab3495_dtp;Uid=ab3495_dtp;Pwd=dtpct123;");
 
         var dummyUserContext = new DummyUserContextService();
 
