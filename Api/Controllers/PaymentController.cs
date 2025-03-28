c16d11639c55b38346714c0bd92d61804f6b163a
kh0abug <kd031212@gmail.com>
2025-03-28 15:28:06 +0700

    Payment-RefactorCreatePayment

--- Api/Controllers/PaymentController.cs
+++ Api/Controllers/PaymentController.cs
@@ -18,41 +18,11 @@ public class PaymentController(
     [HttpPost]
     public async Task<ActionResult> Checkout(PaymentProcessor request)
     {
-        var result = await mediator.Send(new GetOrderDetail(request.BookingId));
-        var link = await CreatePaymentUri(result.Value, request.ResponseUrl);
-        request.PaymentLinkId = link.paymentLinkId;
-        request.NetCost = link.amount;
-        
-        await mediator.Send(request);
+        var result = await mediator.Send(request);
         
         return result.Match<ActionResult>(
-            Some: (value) => Ok(link),
+            Some: Ok,
             None: () => NotFound($"Order ({request.BookingId}) not found."));
-    }
-
-    private async Task<CreatePaymentResult> CreatePaymentUri(OrderDetailResponse order, UriResponse uriResponse)
-    {
-        var items = new List<ItemData>();
-        foreach (var item in order.OrderTickets)
-        {
-            items.Add(new ItemData(item.TicketKind.ToString(), item.Quantity, (int)item.GrossCost));
-        }
-
-        var paymentData = new PaymentData(
-            order.RefCode,
-            (int)order.NetCost,
-            $"DTP Payment",
-            items,
-            uriResponse.CancelUrl,
-            uriResponse.ReturnUrl,
-            null,
-            order.Name,
-            order.Email,
-            order.PhoneNumber
-        );
-
-        var createPayment = await payOs.createPaymentLink(paymentData);
-        return createPayment;
     }
     
     [HttpPost("callback")]
--- Application/Features/Payment/Commands/PaymentProcessor.cs
+++ Application/Features/Payment/Commands/PaymentProcessor.cs
@@ -1,32 +1,77 @@
 ﻿using System.Text.Json.Serialization;
+using Application.Contracts;
 using Application.Contracts.Persistence;
+using Domain.Entities;
 using Domain.Enum;
+using Functional.Option;
 using PaymentEntity = Domain.Entities.Payment;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Net.payOS;
+using Net.payOS.Types;
 
 namespace Application.Features.Payment.Commands;
 
 public record UriResponse(string ReturnUrl, string CancelUrl);
 
-public record PaymentProcessor : IRequest
+public record PaymentProcessor : IRequest<Option<string>>
 {
     public Guid BookingId { get; init; }
-    
-    [JsonIgnore]
-    public string? PaymentLinkId { get; set; }
-    public UriResponse ResponseUrl { get; set; }
-    
-    public decimal NetCost  { get; set; }
+    public required UriResponse ResponseUrl { get; init; }
 
     [JsonIgnore] public PaymentMethod PaymentMethod { get; set; } = PaymentMethod.PayOs;
 }
 
-public class PaymentProcessorHandler(IDtpDbContext context) : IRequestHandler<PaymentProcessor>
+public class PaymentProcessorHandler(IDtpDbContext context, IUserContextService service, PayOS payOs)
+    : IRequestHandler<PaymentProcessor, Option<string>>
 {
-    public async Task Handle(PaymentProcessor request, CancellationToken cancellationToken)
+    public async Task<Option<string>> Handle(PaymentProcessor request, CancellationToken cancellationToken)
     {
-        var payment = new PaymentEntity(request.BookingId, request.PaymentLinkId, request.NetCost);
-        context.Payments.Add(payment);
-        await context.SaveChangesAsync(cancellationToken);
+        var userId = service.GetCurrentUserId()!;
+
+        var order = await context.TourBookings
+            .Include(x => x.Tickets)
+            .ThenInclude(x => x.TicketType)
+            .AsSplitQuery()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == request.BookingId && x.UserId == userId,
+                cancellationToken: cancellationToken);
+
+        if (order is not null)
+        {
+            var link = await CreatePaymentUri(order, request.ResponseUrl);
+            var payment = new PaymentEntity(order.Id, link.paymentLinkId, link.amount);
+            context.Payments.Add(payment);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return link.checkoutUrl;
+        }
+
+        return Option.None;
+    }
+
+    private async Task<CreatePaymentResult> CreatePaymentUri(TourBooking order, UriResponse uriResponse)
+    {
+        var items = new List<ItemData>();
+        foreach (var item in order.Tickets)
+        {
+            items.Add(new ItemData(item.TicketType.ToString() ?? string.Empty, item.Quantity, (int)item.GrossCost));
+        }
+
+        var paymentData = new PaymentData(
+            order.RefCode,
+            (int)order.NetCost(),
+            $"DTP Payment",
+            items,
+            uriResponse.CancelUrl,
+            uriResponse.ReturnUrl,
+            null,
+            order.Name,
+            order.Email,
+            order.PhoneNumber
+        );
+
+        var createPayment = await payOs.createPaymentLink(paymentData);
+        return createPayment;
     }
 }
