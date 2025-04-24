d65056c01e1df346fbcefce03c71ce6404c9e191
kh0abug <kd031212@gmail.com>
2025-04-24 14:04:39 +0700

    Common-HangfireAuthFilter

---
+++ Api/Filters/AllowAllDashboardAuthorizationFilter.cs
@@ -0,0 +1,8 @@
+﻿using Hangfire.Dashboard;
+
+namespace Api.Filters;
+
+public class AllowAllDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+{
+    public bool Authorize(DashboardContext context) => true;
+}
