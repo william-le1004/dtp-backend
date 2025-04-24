b6896de66e5973939820268eddfd3cb89c1b0715
william-le1004 <kienle4456@gmail.com>
2025-04-24 21:53:10 +0700

    Test

--- Application/Features/Tour/Queries/GetTourDetail.cs
+++ Application/Features/Tour/Queries/GetTourDetail.cs
@@ -13,20 +13,11 @@ public class GetTourDetailHandler(IDtpDbContext context) : IRequestHandler<GetTo
 {
     public Task<Option<TourDetailResponse>> Handle(GetTourDetail request, CancellationToken cancellationToken)
     {
-        var tourDetailResponse = context.Tours.IsDeleted(false)
-            .Include(t => t.Ratings)
-            .Include(t => t.Company)
-            .Include(t => t.Tickets)
-            .Include(t => t.TourDestinations)
-            .ThenInclude(td => td.Destination)
-            .Include(t => t.TourDestinations)
-            .ThenInclude(td => td.DestinationActivities)
-            .AsSplitQuery()
-            .AsNoTracking()
-            .Where(t => t.Id == request.TourId)
-            .Select(t => new TourDetailResponse
+        var tourDetailResponse = (from t in context.Tours.IsDeleted(false)
+            where t.Id == request.TourId
+            select new TourDetailResponse
             {
-                Tour = new TourTemplateDetailsResponse()
+                Tour = new TourTemplateDetailsResponse
                 {
                     Id = t.Id,
                     Title = t.Title,
@@ -39,13 +30,12 @@ public class GetTourDetailHandler(IDtpDbContext context) : IRequestHandler<GetTo
                     TicketTypes = t.Tickets,
                     Pickinfor = t.Pickinfor,
                     Include = t.Include,
-                    ImageUrls = context.ImageUrls
-                        .Where(img => img.RefId == t.Id)
-                        .Select(img => img.Url)
-                        .ToList()
+                    ImageUrls = (from img in context.ImageUrls
+                        where img.RefId == t.Id
+                        select img.Url).ToList()
                 },
-                TourDestinations = t.TourDestinations
-                    .Select(td => new TourDestinationResponse
+                TourDestinations = (from td in t.TourDestinations
+                    select new TourDestinationResponse
                     {
                         Name = td.Destination.Name,
                         Longitude = td.Destination.Longitude,
@@ -54,21 +44,19 @@ public class GetTourDetailHandler(IDtpDbContext context) : IRequestHandler<GetTo
                         SortOrderByDate = td.SortOrderByDate,
                         StartTime = td.StartTime,
                         EndTime = td.EndTime,
-                        ImageUrls = context.ImageUrls
-                            .Where(img => img.RefId == td.Id)
-                            .Select(img => img.Url)
-                            .ToList(),
-                        Activities = td.DestinationActivities.Select(x => new TourActivity()
-                        {
-                            Name = x.Name,
-                            StartTime = x.StartTime,
-                            EndTime = x.EndTime,
-                            SortOrder = x.SortOrder ?? 0
-                        }).ToList(),
-                    })
-                    .ToList()
-            })
-            .SingleOrDefault();
+                        ImageUrls = (from img in context.ImageUrls
+                            where img.RefId == td.Id
+                            select img.Url).ToList(),
+                        Activities = (from x in td.DestinationActivities
+                            select new TourActivity
+                            {
+                                Name = x.Name,
+                                StartTime = x.StartTime,
+                                EndTime = x.EndTime,
+                                SortOrder = x.SortOrder ?? 0
+                            }).ToList()
+                    }).ToList()
+            }).SingleOrDefault();
 
         return Task.FromResult(tourDetailResponse is null
             ? Option.None
