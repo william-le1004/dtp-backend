d10ad48ae6120395747de060950786c5f91681e7
kh0abug <kd031212@gmail.com>
2025-05-01 18:51:33 +0000

    Voucher-PlaceOrder

--- Application/Features/Order/Commands/PlaceOrder.cs
+++ Application/Features/Order/Commands/PlaceOrder.cs
@@ -33,7 +33,8 @@ public class PlaceOrderCommandHandler(
     IPublisher publisher,
     IUserContextService userService,
     ITourScheduleRepository repository,
-    IOrderJobService backgroundJob
+    IOrderJobService backgroundJob,
+    IVoucherRepository voucherRepository
     )
     : IRequestHandler<PlaceOrderCommand, TourBooking>
 {
@@ -43,8 +44,7 @@ public class PlaceOrderCommandHandler(
 
         var removeBasketEvent = new OrderSubmittedEvent(userId, request.TourScheduleId);
         var touSchedule = await repository.GetTourScheduleByIdAsync(request.TourScheduleId);
-        var voucher = await context.Voucher.SingleOrDefaultAsync(x => x.Code == request.VoucherCode,
-            cancellationToken: cancellationToken);
+        var voucher = await voucherRepository.GetVoucherByCodeAsync(request.VoucherCode);
 
         var booking = new TourBooking(userId, request.TourScheduleId, touSchedule!,
             request.Name, request.PhoneNumber, request.Email);
