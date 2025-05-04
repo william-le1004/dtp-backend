f571e8435f1205c59f4c9ce4b723cccf1a19c7b0
kh0abug <kd031212@gmail.com>
2025-05-04 09:11:47 +0000

    Wallet-TransactionCode

--- Application/Features/Wallet/Queries/GetTransactionHistory.cs
+++ Application/Features/Wallet/Queries/GetTransactionHistory.cs
@@ -15,7 +15,7 @@ public record TransactionResponse
 
     public TransactionType Type { get; set; }
     
-    public Guid TransactionId { get; init; }
+    public string TransactionId { get; init; }
     
     public DateTime CreatedAt { get; set; }
 };
@@ -42,7 +42,7 @@ public class GetTransactionHistoryHandler(IDtpDbContext context, IUserContextSer
                 Amount = x.Amount,
                 CreatedAt = x.CreatedAt,
                 Type = x.Type,
-                TransactionId = x.Id
+                TransactionId = x.TransactionCode
             });
 
         return Task.FromResult(transactions.AsQueryable());
