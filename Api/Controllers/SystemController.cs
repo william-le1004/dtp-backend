226ebc32e3443a70ff25cf02fba86180895a54bf
kh0abug <kd031212@gmail.com>
2025-05-09 16:21:33 +0000

    System-Setting

---
+++ Api/Controllers/SystemController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain.DataModel;
+using Infrastructure.Contexts;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SystemController(DtpDbContext context) : ControllerBase
+{
+    // GET: api/System
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<SystemSettingDto>>> GetSystemSetting()
+    {
+        var result = await context.SystemSetting.Select(x => new SystemSettingDto()
+        {
+            Id = x.Id,
+            SettingValue = x.SettingValue,
+            SettingCode = MapSettingCodeToVn(x.SettingCode),
+            SettingKey = MapSettingKeyToVn(x.SettingKey)
+        }).ToListAsync();
+        return result;
+    }
+
+    // PUT: api/System/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut]
+    public async Task<IActionResult> PutSystemSetting(List<SystemSettingDto> dtos)
+    {
+        var settingIds= dtos.Select(x => x.Id).ToList();
+        var oldSetting = context.SystemSetting.Where(x => settingIds.Contains(x.Id)).ToList();
+        
+        foreach (var setting in oldSetting)
+        {
+            setting.SettingValue = dtos.FirstOrDefault(x => x.Id == setting.Id)?.SettingValue ?? setting.SettingValue;
+        }
+        await context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    private string MapSettingCodeToVn(SettingCode settingCode)
+    {
+        return settingCode switch
+        {
+            SettingCode.Refund => "Hoàn tiền",
+            _ => string.Empty
+        };
+    }
+
+    private string MapSettingKeyToVn(SettingKey settingKey)
+    {
+        return settingKey switch
+        {
+            SettingKey.FreeCancellationPeriod => "Thời gian(date) miễn phí hoàn tiền",
+            SettingKey.CancelFee => "Phần trăm hoàn tiền chịu phí",
+            SettingKey.NonRefundablePeriod => "Thời gian(date) không hoàn tiền(truớc ngày bắt đầu tour)",
+            _ => string.Empty
+        };
+    }
+}
+
+public record SystemSettingDto
+{
+    public Guid Id { get; set; }
+    public string SettingCode { get; set; }
+    public string SettingKey { get; set; }
+    public long SettingValue { get; set; }
+}
--- Domain/DataModel/SystemSetting.cs
+++ Domain/DataModel/SystemSetting.cs
@@ -3,10 +3,19 @@ namespace Domain.DataModel;
 public class SystemSetting
 {
     public Guid Id { get; set; }
-    public string SettingCode { get; set; }
-    public string SettingKey { get; set; }
-    public string SettingCharValue { get; set; }
-    public string SettingIntegerValue { get; set; }
-    public string SettingDoubleValue { get; set; }
-    public string SettingDecimalValue { get; set; }
+    public SettingCode SettingCode { get; set; }
+    public SettingKey SettingKey { get; set; }
+    public long SettingValue { get; set; }
+}
+
+public enum SettingCode
+{
+    Refund,
+}
+
+public enum SettingKey
+{
+    FreeCancellationPeriod,
+    CancelFee,
+    NonRefundablePeriod,
 }
